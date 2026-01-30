dc94503461747c3213c3a1d19c7db87081f95dd0
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-30 17:27:43 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-cd /var/www/Ecommerce-frontend-react-main
-sudo nginx -t
-sudo systemctl start nginx
+set -e
+
+AWS_REGION=us-east-1
+AWS_ACCOUNT_ID=YOUR_ACCOUNT_ID
+IMAGE_NAME=ecommerce-frontend
+
+echo "Logging in to ECR..."
+aws ecr get-login-password --region $AWS_REGION | \
+docker login --username AWS --password-stdin \
+$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+echo "Pulling latest frontend image..."
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
+
+echo "Running frontend container..."
+docker run -d \
+  --name ecommerce-frontend \
+  -p 3000:80 \
+  --restart always \
+  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
 
