7e68b9f4c03ef59ffc72d7a650051c945c638941
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-31 17:19:59 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,9 +3,12 @@ set -e
 
 echo "Stopping existing frontend container..."
 
-docker stop reactcon || true
-docker rm -f reactcon || true
+docker stop ecommerce-frontend || true
+docker rm -f ecommerce-frontend || true
+docker rmi app
 
 echo "Cleaning Docker system..."
 docker system prune -af || true
 
+cd /var/www/Ecommerce-frontend-react
+rm -rf * .*
